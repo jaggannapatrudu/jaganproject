391aaf3e9bd04f9aa8513aced4c4da89cbf08010
jaggannapatrudu <jagganna.pat@gmail.com>
2026-07-07 10:06:46 +0530

    jagan commit

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test'
 
-test('App login', async({page,context})=>{
-    
+test('App login', async({page})=>{
+
         await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
         let all = page.locator("(//*[@id='product'])[2]/tbody/tr")
         let val = await all.filter({hasText:'Chennai'}).all()
