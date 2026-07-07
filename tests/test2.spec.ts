ddaeed93010af452c6ea6f1a820cea7f6e8c05c1
jaggannapatrudu <jagganna.pat@gmail.com>
2026-07-07 10:45:53 +0530

    dd

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -2,11 +2,5 @@ import {test, expect} from '@playwright/test'
 
 test('App login', async({page})=>{
 
-        await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
-        let all = page.locator("(//*[@id='product'])[2]/tbody/tr")
-        let val = await all.filter({hasText:'Chennai'}).all()
-        for(let vall of val)
-        {
-            console.log(await vall.locator('td').nth(0).innerText())
-        }
+        console.log('jagan branch done some chnages')
 })
