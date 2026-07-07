2169e05331988f6f38c33140e055405de8e7eb7a
jaggannapatrudu <jagganna.pat@gmail.com>
2026-07-07 10:43:36 +0530

    dd

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -9,7 +9,4 @@ test('App login', async({page})=>{
         {
             console.log(await vall.locator('td').nth(0).innerText())
         }
-        console.log('doing changes in main branch')
-        console.log('doing changes in jagan branch')
-        console.log('doing changes in jagan1 branch')
 })
