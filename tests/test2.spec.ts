a8938df298e1277d8653e104ff3d91ee163f7dc0
jaggannapatrudu <jagganna.pat@gmail.com>
2026-07-07 10:27:39 +0530

    :commit

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -13,3 +13,13 @@ test('App login', async({page})=>{
         console.log('doing changes in jagan branch')
         console.log('doing changes in jagan1 branch')
 })
+test('Nihanya login', async({page})=>{
+
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+        let all = page.locator("(//*[@id='product'])[2]/tbody/tr")
+        let val = await all.filter({hasText:'Chennai'}).all()
+        for(let vall of val)
+        {
+            console.log(await vall.locator('td').nth(0).innerText())
+        }
+})
